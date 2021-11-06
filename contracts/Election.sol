993c8db887aa9b4e58f6818069d38f6fbcf3e1d6
Lucas Vieira <vieiralucas4@gmail.com>
2021-11-06 15:47:05 -0300

    Initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+contract Election {
+  // Store candidate
+  // Read candidate
+  string public candidate;
+  // Constructor
+  constructor() public {
+    candidate = "Candidate 1";
+  }
+}
